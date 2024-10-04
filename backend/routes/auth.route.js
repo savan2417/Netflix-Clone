22b1f7b34b0f8c9ea10a203c5922c952c559d3d1
savan2417 <savanrathod9876@gmail.com>
2024-10-04 23:03:07 +0530

    Initial commit

---
+++ backend/routes/auth.route.js
@@ -0,0 +1,13 @@
+import express from "express";
+import { protectRoute } from "../middleware/protectRoute.js";
+import { login, logout, signup } from "../controller/auth.controller.js";
+
+const router = express.Router();
+
+router.post("/signup", signup);
+router.post("/login", login);
+router.post("/logout", logout);
+
+// router.get("/authCheck", protectRoute, authCheck);
+
+export default router;
